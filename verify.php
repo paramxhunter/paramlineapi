681bf67f22014c74b2babb5b5a87c38b37d63b5d
paramxhunter <52811366+paramxhunter@users.noreply.github.com>
2019-07-12 14:57:55 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'rHB/F5hC2BkzpZB+vhH99A7xulLq9CgmoX92yJEiLBvfA0llHlD4XqUTVU4JOB2Xo3tNnFuKr93Bbzdd91Y1G34tSedTTn2rVLK+VdR4HX53qoaJlbqPtlXbWdaSH1NHTyeJZqkMYTlKN2xDef0eKQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
